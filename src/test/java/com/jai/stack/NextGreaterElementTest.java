50cc4ca4b7c2e9307e4e83e32ebda9edd424fd7a
jaibhardwaj <jai.bhardwaj99@gmail.com>
2025-08-04 21:40:02 +0530

    Implemented NextGreaterElement with unit test

---
+++ src/test/java/com/jai/stack/NextGreaterElementTest.java
@@ -0,0 +1,49 @@
+package com.jai.stack;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NextGreaterElementTest
+{
+	@Test
+	void testNextGreaterElement_basic()
+	{
+		int[] arr = {4, 5, 2, 25};
+		int[] expected = {5, 25, 25, -1};
+		assertArrayEquals(expected, NextGreaterElement.nextGreaterElement(arr));
+	}
+
+	@Test
+	void testNextGreaterElement_allDescending()
+	{
+		int[] arr = {9, 7, 5, 3};
+		int[] expected = {-1, -1, -1, -1};
+		assertArrayEquals(expected, NextGreaterElement.nextGreaterElement(arr));
+	}
+
+	@Test
+	void testNextGreaterElement_allSame()
+	{
+		int[] arr = {2, 2, 2, 2};
+		int[] expected = {-1, -1, -1, -1};
+		assertArrayEquals(expected, NextGreaterElement.nextGreaterElement(arr));
+	}
+
+	@Test
+	void testNextGreaterElement_singleElement()
+	{
+		int[] arr = {10};
+		int[] expected = {-1};
+		assertArrayEquals(expected, NextGreaterElement.nextGreaterElement(arr));
+	}
+
+	@Test
+	void testNextGreaterElement_empty()
+	{
+		int[] arr = {};
+		int[] expected = {};
+		assertArrayEquals(expected, NextGreaterElement.nextGreaterElement(arr));
+	}
+}
+
